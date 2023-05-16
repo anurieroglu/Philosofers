f903184b8cee3158edcd1b7a9040f536d881f332
Ahmet nuri Eroglu <aeroglu@k1m07s08.42istanbul.com.tr>
2023-05-16 20:23:38 +0300

    first

---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aeroglu <aeroglu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 17:31:43 by aeroglu           #+#    #+#             */
+/*   Updated: 2023/05/16 17:35:45 by aeroglu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
